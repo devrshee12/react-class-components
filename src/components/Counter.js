e3d103f1986dcc18179a33aada19c70318f2b1d3
Panchhiwala Devarshee <84611501+devrshee12@users.noreply.github.com>
2024-01-10 11:03:29 +0530

    done some changes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,6 +6,8 @@ class Counter extends Component{
         this.state = {
             count: 1,
         }
+        this.handleIncrease = this.handleIncrease.bind(this); // do this in constructor to bind this
+        this.handleDecrease = this.handleDecrease.bind(this);
     }
 
     componentDidMount(){
@@ -19,29 +21,55 @@ class Counter extends Component{
     componentWillUnmount(){
         console.log("component will unmount called in counter components");
     }
-    render() {
-        const handleIncrease = () => {
-            this.setState({
-                count: this.state.count + 1
+
+    handleIncrease(){
+        // console.log(this);
+        this.setState(function(state, props){
+            return{
+                count: state.count + 1
+            }
+        })
+    }
+
+    handleDecrease(){
+        // console.log(this);
+        if(this.state.count - 1 < 0){
+            alert("You can not decrease count");
+        }
+        else{
+            // if you are updaing multiple states it will be async so do it like this 
+            this.setState((state, props) => { // always use like this 
+                return {
+                    count: state.count - 1
+                }
             })
         }
-        const handleDecrease = () => {
-            if(this.state.count - 1 < 0){
-                alert("you can not decrease");
-            }
-            else{
-                this.setState({
-                    count: this.state.count - 1
-                })
-            }
-        }
+    }
+
+
+    render() {
+        // const handleIncrease = () => {
+        //     this.setState({
+        //         count: this.state.count + 1
+        //     })
+        // }
+        // const handleDecrease = () => {
+        //     if(this.state.count - 1 < 0){
+        //         alert("you can not decrease");
+        //     }
+        //     else{
+        //         this.setState({
+        //             count: this.state.count - 1
+        //         })
+        //     }
+        // }
         return (
             <>
                 <div className="App-header">
                     <h3>{this.state.count}</h3>
                     <div className="btn-container">
-                    <button className="btn" onClick={handleIncrease}>Increase</button>
-                    <button className="btn" onClick={handleDecrease}>Decrease</button>
+                    <button className="btn" onClick={this.handleIncrease}>Increase</button>
+                    <button className="btn" onClick={this.handleDecrease}>Decrease</button>
                     </div>
                 </div>
             </>
